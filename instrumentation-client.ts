c595e659a25b3ef71c05f650e913d5043167b9ef
Octavian <mihai.g.octavian24@stud.rau.ro>
2025-10-27 11:49:54 +0200

    fix(config): enhance Sentry instrumentation and error handling
    Improves Sentry client configuration by refining error filtering,
      adding environment context, and preventing event sending in dev.
    
    Changes:
      - Created instrumentation-client.ts with complete client setup
      - Added onRequestError hook in instrumentation.ts
      - Added onRouterTransitionStart for navigation tracking
      - Configured Session Replay with privacy settings
      - Removed invalid telemetry option from all configs
    
    Related: Build warnings cleanup

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,10 @@
 // This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
+// The config you add here will be used whenever a user loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
 
-// Export hook for navigation instrumentation (required by Sentry SDK)
+// Export hook for router transition tracking
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
 
 Sentry.init({
@@ -15,6 +15,8 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  environment: process.env.NODE_ENV,
 
   replaysOnErrorSampleRate: 1.0,
 
@@ -31,51 +33,27 @@ Sentry.init({
     }),
   ],
 
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-
-  environment: process.env.NODE_ENV,
-
-  // Ignore specific errors
+  // Ignore specific errors on client
   ignoreErrors: [
-    // Random plugins/extensions
+    // Browser extensions
     "top.GLOBALS",
-    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
-    "originalCreateNotification",
-    "canvas.contentDocument",
-    "MyApp_RemoveAllHighlights",
-    "http://tt.epicplay.com",
+    "chrome-extension://",
+    "moz-extension://",
+    // Random plugins/extensions
     "Can't find variable: ZiteReader",
     "jigsaw is not defined",
     "ComboSearch is not defined",
-    "http://loading.retry.widdit.com/",
-    "atomicFindClose",
-    // Facebook borked
-    "fb_xd_fragment",
-    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
-    // reduce this. (thanks @acdha)
-    // See http://stackoverflow.com/questions/4113268
-    "bmi_SafeAddOnload",
-    "EBCallBackMessageReceived",
-    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
-    "conduitPage",
+    // Network errors
+    "NetworkError",
+    "Non-Error promise rejection captured",
   ],
 
-  denyUrls: [
-    // Facebook flakiness
-    /graph\.facebook\.com/i,
-    // Facebook blocked
-    /connect\.facebook\.net\/en_US\/all\.js/i,
-    // Woopra flakiness
-    /eatdifferent\.com\.woopra-ns\.com/i,
-    /static\.woopra\.com\/js\/woopra\.js/i,
-    // Chrome extensions
-    /extensions\//i,
-    /^chrome:\/\//i,
-    // Other plugins
-    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
-    /webappstoolbarba\.texthelp\.com\//i,
-    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
-  ],
+  beforeSend(event, hint) {
+    // Don't send events for development
+    if (process.env.NODE_ENV === "development") {
+      console.error(hint.originalException || hint.syntheticException);
+      return null;
+    }
+    return event;
+  },
 });
